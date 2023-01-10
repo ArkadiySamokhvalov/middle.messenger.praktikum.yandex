bd228f63f5580987df92ef1ba1f864d3bfd9c078
ArkadiySamokhvalov <arkadii.samohvalov@bk.ru>
2023-01-10 20:31:10 +0300

    feat: add store

---
+++ src/utils/Store.ts
@@ -0,0 +1,30 @@
+import set from './helpers/set';
+import EventBus from './EventBus';
+
+export enum StoreEvents {
+  Updated = 'updated',
+}
+
+export default class Store extends EventBus {
+  private static __instance: Store;
+  private _state: any = {};
+
+  constructor() {
+    super();
+
+    if (Store.__instance) {
+      return Store.__instance;
+    }
+
+    Store.__instance = this;
+  }
+
+  public getState() {
+    return this._state;
+  }
+
+  public set(keypath: string, data: unknown) {
+    set(this.getState(), keypath, data);
+    this.emit(StoreEvents.Updated, this.getState());
+  }
+}
