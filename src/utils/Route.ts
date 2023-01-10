1e6c71366cb31689682c64e6805dfb3007aa848f
ArkadiySamokhvalov <arkadii.samohvalov@bk.ru>
2023-01-10 20:30:16 +0300

    feat: add routing

---
+++ src/utils/Route.ts
@@ -0,0 +1,37 @@
+import Block from './Block';
+import render from './renderDOM';
+
+export default class Route {
+  private _block: Block | null = null;
+
+  constructor(
+    private _pathname: string,
+    private readonly _blockClass: typeof Block,
+    private readonly _props: Record<string, unknown>
+  ) {}
+
+  public navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  public leave() {
+    if (this._block) {
+      this._block = null;
+    }
+  }
+
+  public match(pathname: string) {
+    return pathname === this._pathname;
+  }
+
+  public render() {
+    if (!this._block) {
+      this._block = new this._blockClass();
+      render(this._props.rootQuery as string, this._block);
+      return;
+    }
+  }
+}
