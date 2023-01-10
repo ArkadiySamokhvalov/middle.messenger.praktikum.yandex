9228b78db1ea9b2fe45da93c91ba6c00f4e851ac
ArkadiySamokhvalov <arkadii.samohvalov@bk.ru>
2023-01-10 20:30:16 +0300

    feat: add routing

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+const enum Routes {
+  Index = '/',
+  SignUp = '/sign-up',
+  Chat = '/messenger',
+  UserSettings = '/settings',
+  ChangePassword = '/settings/change-password',
+}
+
+export default Routes;
